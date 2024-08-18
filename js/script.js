4093c40271431d891985bc69c5147f3ed1d23a87
Did-hub-max <did@scuad.tech>
2024-08-18 15:00:21 +0300

    commit 4

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('header-burger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.header__nav').classList.toggle('active');
+  });
+  
